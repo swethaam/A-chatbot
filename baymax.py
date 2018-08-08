31ef024f6b9cf6527b56b0ee4eba62da16018a83
swethaam <42196773+swethaam@users.noreply.github.com>
2018-08-08 18:02:30 +0530

    Update baymax.py

--- baymax.py
+++ baymax.py
@@ -1,9 +1,14 @@
+#use this code if the bot has already been trained else use baytrainer
 import  os
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+#name the chatbox as baymax
 bot = ChatBot("baymax")
+#set up a trainer for baymax
 bot.set_trainer(ListTrainer)
+#once the bot got trained input the chat and get reply from the bot "bay"
 while True:
+#if "bye" is typed exit the program else continue getting reply from the chat 
 	message = input("you:")
 	if message.strip() != "bye":
 		reply = bot.get_response(message)
