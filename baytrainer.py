b82e7b689aa5940db74f9dd670b65b98d891ee80
swethaam <42196773+swethaam@users.noreply.github.com>
2018-08-08 18:07:56 +0530

    Update baytrainer.py

--- baytrainer.py
+++ baytrainer.py
@@ -1,14 +1,20 @@
+#import the chatterbot  
 import  os
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-
+#name the chatterbot as "baymax"
 bot = ChatBot("baymax")
+#set the trainer to "baymax"
 bot.set_trainer(ListTrainer)
+#loop through all the files which are needed to train the bot
 for each in os.listdir("C:/Users/white/Downloads/chatterbot/chatterbot-corpus-master/chatterbot_corpus/data/english"):
 	data = open("C:/Users/white/Downloads/chatterbot/chatterbot-corpus-master/chatterbot_corpus/data/english/"+each,"r").readlines()
+	#train the bot
 	bot.train(data)
+	#once bot got trained completely enter input and get reply from the bot
 while True:
 	message = input("you:")
+	#if user input is "bye" exit the chat else continue the chat
 	if message.strip() != "bye":
 		reply = bot.get_response(message)
 		print("bay:",reply)
